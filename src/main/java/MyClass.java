8d823ed66e6567fb2cb77ff18db38f3e3388b4a9
Ashish Gupta <ashi_goyanka@Ashishs-Air.bbrouter>
2023-07-15 11:38:21 +0530

    Initial commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,26 @@
+public class MyClass {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hellow World");
+    }
+
+    public void m1()
+    {
+        System.out.println("m1 method");
+        int i=0;
+
+    }
+
+    public void m2(int a)
+    {
+        System.out.println("m2 method");
+    }
+
+
+    public void m3()
+    {
+        System.out.println("m3 method");
+    }
+}
